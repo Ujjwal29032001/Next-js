b51f88968be0929e6619aa0a2669d4df4e8ec858
Ujjwal29032001 <ujjukush29@gmail.com>
2025-05-16 14:50:53 +0530

    Next-js

---
+++ demo_app/src/app/components/My.js
@@ -0,0 +1,4 @@
+export default function My() {
+    return<div>
+    </div>
+}
---
+++ demo_app/src/app/page.js
@@ -0,0 +1,9 @@
+import My from "./components/My"
+import {userRouter} from "next/router"
+export default function Home() {
+  return<div>
+    
+    <h1 style={{textAlign:'center',color:'red'}}>Hello Students Welcome to Next Js Batch</h1>
+    <My/>
+  </div>
+}
